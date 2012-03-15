97cd22232d8be5913b374831bf7a4fae8686a659
hailor <hailor1985@gmail.com>
2012-03-15 16:06:47 +0800

    init rainbow project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rainbow::Application.config.secret_token = 'f81dcb14be021b39bdf840748758f41f934238a6ef1be2b163d60f45d41ab49164fc5646d8f94361753e202456bded0537ad5672712a82380171243d40e4c630'
